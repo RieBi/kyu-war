7d6a997bf1b1bad8dcb2efcdc224a174e1d929bc
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-08 17:01:14 +0200

    Make number parser

---
+++ Solutions/Kyu2/EvaluateMathematicalExpressions/Evaluate.cs
@@ -0,0 +1,116 @@
+﻿namespace Evaluation;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+public class Evaluate
+{
+    public string eval(string expression)
+    {
+        string result = "0"; //calculated expression (double converted to string) or Errormessage starting with "ERROR" (+ optional Errormessage)
+        return result;
+    }
+}
+
+public enum TokenKind
+{
+    BinaryOperator,
+    Number,
+    Function,
+    OpeningParenthese,
+    ClosingParenthese
+}
+
+public record Token(TokenKind Kind, string value);
+
+public class Tokenizer(string source)
+{
+    private static ImmutableHashSet<char> numberSign = ['+', '-'];
+
+    private string _source = source;
+    private int _pos = 0;
+    private readonly List<Token> _tokens = [];
+
+    public List<Token> Tokenize()
+    {
+        while (_pos < _source.Length)
+        {
+            if (IsSkippable(_source[_pos]))
+            {
+                _pos++;
+                continue;
+            }
+
+
+        }
+
+        return _tokens;
+    }
+
+    public Token? ParseNumber()
+    {
+        const char decimalSeparator = '.';
+        ImmutableHashSet<char> scientificNotationSeparator = ['e', 'E'];
+
+        if (!IsDigit(At()))
+            return null;
+
+        var start = _pos;
+        var pos = _pos;
+        while (IsDigit(At(pos)))
+            pos++;
+
+        var result = new Token(TokenKind.Number, _source[start..pos]);
+        _pos = pos;
+
+        if (At(pos) == decimalSeparator)
+        {
+            pos++;
+
+            if (!IsDigit(At(pos)))
+                return result;
+
+            while (IsDigit(At(pos)))
+                pos++;
+
+            result = new Token(TokenKind.Number, _source[start..pos]);
+            _pos = pos;
+        }
+
+        if (!scientificNotationSeparator.Contains(At(pos)))
+            return result;
+
+        pos++;
+
+        if (numberSign.Contains(At(pos)))
+            pos++;
+
+        if (!IsDigit(At(pos)))
+            return result;
+
+        while (IsDigit(At(pos)))
+            pos++;
+
+        result = new Token(TokenKind.Number, _source[start..pos]);
+        _pos = pos;
+        return result;
+    }
+
+    private char At() => _pos >= _source.Length ? char.MinValue : _source[_pos];
+
+    private char At(int index) => index >= _source.Length ? char.MinValue : _source[index];
+
+    private bool IsEof() => _pos >= _source.Length;
+
+    private bool IsEof(int index) => index >= _source.Length;
+
+    private static bool IsDigit(char ch) => char.IsAsciiDigit(ch);
+
+    private static bool IsSkippable(char ch)
+    {
+        HashSet<char> skippable = [' ', '\n', '\r', '\t'];
+
+        return skippable.Contains(ch);
+    }
+}
---
+++ Tests/Kyu2/EvaluateMathematicalExpressionsTests/TokenizerTests.cs
@@ -0,0 +1,23 @@
+﻿using Evaluation;
+
+namespace Tests.Kyu2.EvaluateMathematicalExpressionsTests;
+public class TokenizerTests
+{
+    [Theory]
+    [InlineData("123 text", "123")]
+    [InlineData("123.0123", "123.0123")]
+    [InlineData("123.+123", "123")]
+    [InlineData("123e10 text", "123e10")]
+    [InlineData("123e+10text", "123e+10")]
+    [InlineData("555.77e-7 else", "555.77e-7")]
+    [InlineData("1e5.8", "1e5")]
+    public void ParseNumber_ReturnsOnlyNumber(string input, string expected)
+    {
+        var tokenizer = new Tokenizer(input);
+
+        var actual = tokenizer.ParseNumber();
+
+        Assert.Equal(expected, actual?.value);
+        Assert.Equal(TokenKind.Number, actual?.Kind);
+    }
+}
