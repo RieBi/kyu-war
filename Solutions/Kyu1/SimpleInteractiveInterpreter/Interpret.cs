3842775ceb084f66d4c4569658e857fa301b5c9e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-18 20:03:15 +0200

    Add node classes

--- Solutions/Kyu1/SimpleInteractiveInterpreter/Interpret.cs
+++ Solutions/Kyu1/SimpleInteractiveInterpreter/Interpret.cs
@@ -23,3 +23,92 @@ public partial class Interpreter
     [GeneratedRegex(@"=>|[-+*/%=\\(\\)]|[A-Za-z_][A-Za-z0-9_]*|[0-9]+\.?[0-9]+")]
     private static partial Regex MyRegex();
 }
+
+public class Parser
+{
+    private readonly List<string> _tokens;
+    private int _pos = 0;
+
+    public Parser(List<string> tokens)
+    {
+        _tokens = tokens;
+    }
+
+    public void ParseExpression()
+    {
+        var cur = _tokens[_pos++];
+
+        IParserNode curNode;
+
+        if (IsNumber(cur, out var value))
+        {
+            var node = new ValueNode();
+            node.Value = value;
+            curNode = node;
+        }
+    }
+
+    private bool IsNumber(string token, out double result) => double.TryParse(token, out result);
+
+    private string At() => _tokens[_pos];
+
+    private string Next() => _tokens[_pos++];
+}
+
+public interface IParserNode
+{
+    double GetResult();
+}
+
+public class ValueNode : IParserNode
+{
+    public double Value { get; set; }
+
+    public double GetResult() => Value;
+}
+
+public class BinaryExpressionNode : IParserNode
+{
+    public BinaryExpressionNode(IParserNode leftChild, IParserNode rightChild, string binaryOperator)
+    {
+        LeftChild = leftChild;
+        RightChild = rightChild;
+        Operator = binaryOperator;
+    }
+
+    public IParserNode LeftChild { get; set; }
+    public IParserNode RightChild { get; set; }
+    public string Operator { get; set; }
+
+    public double GetResult()
+    {
+        var leftValue = LeftChild.GetResult();
+        var rightValue = RightChild.GetResult();
+
+        var result = Operator switch
+        {
+            "+" => leftValue + rightValue,
+            "-" => leftValue - rightValue,
+            "*" => leftValue * rightValue,
+            "/" => leftValue / rightValue,
+            "%" => leftValue % rightValue,
+            _ => double.NaN
+        };
+
+        return result;
+    }
+}
+
+public class AssignmentNode : IParserNode
+{
+    public AssignmentNode(string identifier, IParserNode rightHandSide)
+    {
+        Identifier = identifier;
+        RightHandSide = rightHandSide;
+    }
+
+    public string Identifier { get; set; }
+    public IParserNode RightHandSide { get; set; }
+
+    public double GetResult() => RightHandSide.GetResult();
+}
